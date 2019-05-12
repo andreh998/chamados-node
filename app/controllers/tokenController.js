2cbe3539a40962da42231773e7da8e740c68011a
André Hoffmann <andreh998@gmail.com>
2019-05-12 19:35:21 -0300

    Ajuste na view de usuários

--- app/controllers/tokenController.js
+++ app/controllers/tokenController.js
@@ -5,12 +5,14 @@ function verifyJWT(req, res, next){
     var token = req.cookies['auth'];
     //console.log(token);
     //verifico se o token existe, se não existir retorno um erro
-    if (!token) return res.status(401).send({auth: false, message: 'No token provided.'});
+    //if (!token) return res.status(401).send({auth: false, message: 'No token provided.'});
+    if (!token) return res.redirect('/logout');
 
     //se o token existir tento decriptar
     jwt.verify(token, process.env.SECRET, function(err, decoded){
         //se der erro para decriptar retorno um erro
-        if(err) return res.status(500).send({auth: false, message: 'Failed to authenticate token.'});
+        //if(err) return res.status(500).send({auth: false, message: 'Failed to authenticate token.'});
+        if(err) return res.redirect('/logout');
 
         //se ok, salva no request para uso posterior
         req.userId = decoded.id;
--- app/routes/configUsuarios.js
+++ app/routes/configUsuarios.js
@@ -1,5 +1,5 @@
 module.exports = function(application){
-    application.get('/configuracoes/usuarios', function(req, res){
+    application.get('/config/usuarios', function(req, res){
         application.app.controllers.usuariosController.index(application, req, res);
     });
 }
